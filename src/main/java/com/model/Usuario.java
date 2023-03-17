f1b64cb4508562821077c949e8755d74e1c35231
Caroline Souza <loh_jg@hotmail.com>
2023-03-17 16:13:47 -0300

    mapeando as entidades

---
+++ src/main/java/com/model/Usuario.java
@@ -0,0 +1,144 @@
+package com.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+@Entity
+public class Usuario implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Temporal(TemporalType.DATE)
+	private Date data;
+	
+	private String nome;
+	
+	@CPF
+	private String cpf;
+	
+	private String email;
+	
+	@ManyToOne()
+	@JoinTable()
+	private Perfil perfil;
+	
+	@ManyToMany()
+	@JoinTable()
+	private Endereco enderecos;
+
+	public Usuario(Long id, Date data, String nome, String cpf, String email, Perfil perfil, Endereco enderecos) {
+		super();
+		this.id = id;
+		this.data = data;
+		this.nome = nome;
+		this.cpf = cpf;
+		this.email = email;
+		this.perfil = perfil;
+		this.enderecos = enderecos;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Perfil getPerfil() {
+		return perfil;
+	}
+
+	public void setPerfil(Perfil perfil) {
+		this.perfil = perfil;
+	}
+
+	public Endereco getEnderecos() {
+		return enderecos;
+	}
+
+	public void setEnderecos(Endereco enderecos) {
+		this.enderecos = enderecos;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+		
+	
+	
+	
+	
+	
+	
+	
+
+}
