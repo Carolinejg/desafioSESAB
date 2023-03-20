7eb9533c6fe00a09d5075ff31e1cf93707b540b7
Caroline Souza <loh_jg@hotmail.com>
2023-03-20 14:36:08 -0300

    ordenacao

--- src/main/resources/META-INF/sql/dados-iniciais.sql
+++ src/main/resources/META-INF/sql/dados-iniciais.sql
@@ -6,6 +6,8 @@ INSERT INTO endereco (cep,logradouro) VALUES ("40230120", "Engenho velho");
 
 INSERT INTO usuario (cpf,data,email,nome,perfil_id) VALUES ("06421140569","2015/03/25","loh_jg@hotmail.com","Caroline",1);
 INSERT INTO usuario (cpf,data,email,nome,perfil_id) VALUES ("064211405009","2015/09/01","carryuneb@hotmail.com","Jesus",2);
+INSERT INTO usuario (cpf,data,email,nome,perfil_id) VALUES ("06421140509","2015/09/01","carryuneb@hotmail.com","Ana",2);
+
 
 INSERT INTO usuario_endereco (usuario_id,endereco_id) VALUES (1,1);
 INSERT INTO usuario_endereco (usuario_id,endereco_id) VALUES (2,2);
